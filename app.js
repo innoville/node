ada2e922aeda2181ce91efbc0cdfb8bde2e6eb97
Cho <choyon@186590d03b81.ant.amazon.com>
2018-02-19 22:06:20 +0900

    + app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express);
+var app = express();
+app.get('/', function (req, res){
+  res.sendFile('./index.html');
+});
+app.listen(8080, function(){
+  console.log('APP Listening on port 8080');
+});
