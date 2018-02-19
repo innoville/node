9b5269064b8f99b8668ade0edc475b0a56bf5c23
Cho <choyon@186590d03b81.ant.amazon.com>
2018-02-19 22:43:02 +0900

    x app.js

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+});
+
+console.log(process.env.RDS_USERNAME);
+
+connection.connect(function(err){
+  if(err) {
+    console.log("DB Connection Error");
+    return;
+  }
+  console.log("DB Connected");
+});
+connection.end();
 app.get('/', function (req, res){
   console.log(__dirname);
   res.sendFile(__dirname+'/index.html');
